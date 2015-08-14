eecf1f0e47dc55f1cb8887c613d4dc7579596008
Hyunsoo <ppnchb@outlook.com>
2015-08-15 00:03:56 +0900

    Object splitting added Object/Block prefab added Block collider width modified

---
+++ Assets/Scripts/BlockDestroyTesterScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDestroyTesterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0)) {
+			BlockDestroyerScript d = transform.GetComponent<BlockDestroyerScript>();
+			d.destroyBlock();
+		}
+	}
+}
---
+++ Assets/Scripts/BlockDestroyerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockDestroyerScript : MonoBehaviour {
+
+	private ObjectSplitterScript obj;
+
+	// Use this for initialization
+	void Start () {
+		obj = transform.parent.GetComponent<ObjectSplitterScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = this.transform.position.x;
+		float y = this.transform.position.y;
+		float z = this.transform.position.z;
+		this.transform.position.Set (Mathf.Round (x), y, z);
+	}
+
+	public void destroyBlock() {
+		Object.Destroy (this.gameObject);
+		obj.split (this.transform);
+
+	}
+}
